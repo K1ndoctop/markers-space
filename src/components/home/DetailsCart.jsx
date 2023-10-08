1f19f921acc5432506a7faef1ae8d9a88ed33dfb
kjbuhyvbhxhcbiusgrelgmkef <malik.vundiz@gmail.com>
2023-10-08 15:31:07 +0600

    f

--- src/components/home/DetailsCart.jsx
+++ src/components/home/DetailsCart.jsx
@@ -1,51 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getOneEvent } from "../../store/event/eventAction";
+import { useParams } from "react-router-dom";
 
 const DetailsCart = () => {
+  const { oneEvent, loading, error } = useSelector((state) => state.events);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(getOneEvent(id));
+  }, [id]); // Включите 'id' в массив зависимостей
+
+  // Проверка наличия данных
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!oneEvent) {
+    return <div>No data found</div>;
+  }
+
+  // Дополнительная проверка полей oneEvent перед их отображением
+  const { name, fulldescrip } = oneEvent;
+
   return (
-    <div className="flex flex-row newColor p-8 items-centerw w-2/3 rounded-xl justify-center m-auto">
-      {/* <img
-        src="https://www.5.ua/media/pictures/400x266/190482.jpg?t=1596524788"
-        alt=""
-        className="w-96 h-96"
-      /> */}
+    <div className="flex flex-row newColor p-8 items-center w-2/3 rounded-xl justify-center m-auto">
       <div className="ml-5">
-        <h2 className="text-3xl font-bold">Lorem ipsum dolor sit.</h2>
-        <p className="text-xl font-bold ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          consequuntur soluta id quidem, molestias quia odio suscipit aperiam,
-          architecto praesentium similique impedit, nesciunt hic debitis ea
-          accusamus tenetur! Impedit, perferendis! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Explicabo alias sapiente quod
-          consequatur optio? Quibusdam harum, officiis nemo voluptatem dolores,
-          cumque placeat repellendus magnam laborum id hic reprehenderit quae
-          repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Error rem soluta ducimus maiores, voluptatibus facilis ipsa nesciunt
-          omnis! Autem velit ratione aliquid saepe explicabo nobis voluptatibus
-          ad eveniet exercitationem ducimus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          consequuntur soluta id quidem, molestias quia odio suscipit aperiam,
-          architecto praesentium similique impedit, nesciunt hic debitis ea
-          accusamus tenetur! Impedit, perferendis! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Explicabo alias sapiente quod
-          consequatur optio? Quibusdam harum, officiis nemo voluptatem dolores,
-          cumque placeat repellendus magnam laborum id hic reprehenderit quae
-          repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Error rem soluta ducimus maiores, voluptatibus facilis ipsa nesciunt
-          omnis! Autem velit ratione aliquid saepe explicabo nobis voluptatibus
-          ad eveniet exercitationem ducimus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          consequuntur soluta id quidem, molestias quia odio suscipit aperiam,
-          architecto praesentium similique impedit, nesciunt hic debitis ea
-          accusamus tenetur! Impedit, perferendis! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Explicabo alias sapiente quod
-          consequatur optio? Quibusdam harum, officiis nemo voluptatem dolores,
-          cumque placeat repellendus magnam laborum id hic reprehenderit quae
-          repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Error rem soluta ducimus maiores, voluptatibus facilis ipsa nesciunt
-          omnis! Autem velit ratione aliquid saepe explicabo nobis voluptatibus
-          ad eveniet exercitationem ducimus?
-          
-        </p>
+        <h2 className="text-3xl font-bold">{name}</h2>
+        <p className="text-xl font-bold ">{fulldescrip}</p>
         <button>Close</button>
       </div>
     </div>
